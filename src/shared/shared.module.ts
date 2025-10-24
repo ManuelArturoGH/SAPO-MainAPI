2f02b87e8331f1e6720e596097916a11506cecee
Manuel Arturo Guillen Hernandez <60072912+ManuelArturoGH@users.noreply.github.com>
2025-10-24 11:17:44 -0600

    Delete src directory

--- src/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { RequestQueueService } from './request-queue.service';
-
-@Global()
-@Module({
-  providers: [RequestQueueService],
-  exports: [RequestQueueService],
-})
-export class SharedModule {}
